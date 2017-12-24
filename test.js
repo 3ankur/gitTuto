43d8dd784d016e00b8cb0ccbb4d6a0e5598c0bdc
unknown <3ankur.v@gmail.com>
2017-12-24 22:38:46 +0530

    Added the js file for user #ForkUser-Ankur

---
+++ test.js
@@ -0,0 +1,14 @@
+function User(data){
+	
+	this.name = data.name;
+	this.age = data.age;
+	
+	this.getUserDetail = function(){
+		
+		return "Name:-"+this.name +"   "+"Age:"+this.age;
+	}
+	
+}
+
+User ankur = new User({name:"Ankur",age:27});
+console.log(ankur.getUserDetail());
